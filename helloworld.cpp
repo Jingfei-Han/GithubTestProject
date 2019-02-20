8e11c296219e96d8627b3d4c17a12bc5f48add4e
Jingfei Han <jinha@microsoft.com>
2019-02-20 23:17:26 +0800

    Add function add

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,6 +1,14 @@
 #include<stdio.h>
+int add(int a, int b)
+{
+	return a + b;
+}
 int main()
 {
-	printf("Hello world!\n");
+	int a, b, c;
+	scanf("%d%d", &a, &b);
+	c = a + b;
+	printf("%d\n", c);
+	printf("hello world!\n");
 	return 0;
 }
